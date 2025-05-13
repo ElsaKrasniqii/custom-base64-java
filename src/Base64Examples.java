21be18bfd9dce7683d1465e9d3289bca6f0e9d6e
ElonaB-404 <elona.biringjiku@student.uni-pr.edu>
2025-05-13 18:56:25 -0200

    Krijimi i funksionit me shembuj te Base64

--- src/Base64Examples.java
+++ src/Base64Examples.java
@@ -1,2 +1,14 @@
+
 public class Base64Examples {
+    public static void shfaqShembuj() {
+        String shembull = "Pershendetje!";
+        String eKoduar = Base64Encoder.encode(shembull.getBytes());
+        String eDekoduar = new String(Base64Decoder.decode(eKoduar));
+
+        System.out.println("Teksti origjinal: " + shembull);
+        System.out.println("E koduar: " + eKoduar);
+        System.out.println("E dekoduar: " + eDekoduar);
+    }
+
+
 }
