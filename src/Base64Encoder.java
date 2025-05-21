f21c4c12684aa5b72da502ca76e8c1041d95af33
ElsaKrasniqii <elsa.krasniqi12@student.uni-pr.edu>
2025-05-21 13:08:01 +0200

    Ndryshimet e bera tek files

--- src/Base64Encoder.java
+++ src/Base64Encoder.java
@@ -1,4 +1,6 @@
+
 public class Base64Encoder {
+
     private static final char[] BASE64_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".toCharArray();
 
     public static String encode(byte[] input) {
@@ -7,6 +9,7 @@ public class Base64Encoder {
 
         while (i < input.length) {
             int remaining = input.length - i;
+
             int b = (input[i++] & 0xFF) << 16;
 
             if (remaining > 1) b |= (input[i++] & 0xFF) << 8;
@@ -14,27 +17,11 @@ public class Base64Encoder {
 
             result.append(BASE64_CHARS[(b >> 18) & 0x3F]);
             result.append(BASE64_CHARS[(b >> 12) & 0x3F]);
-            result.append(remaining > 1 ? BASE64_CHARS[(b >> 6) & 0x3F] : '=');
-            result.append(remaining > 2 ? BASE64_CHARS[b & 0x3F] : '=');
+            result.append(remaining > 1 ? BASE64_CHARS[(b >> 6) & 0x3F] : '?');
+            result.append(remaining > 2 ? BASE64_CHARS[b & 0x3F] : '?');
         }
 
         return result.toString();
     }
 
-    public static String encodeWithInvalidCharsIgnored(String input) {
-        StringBuilder result = new StringBuilder();
-
-        for (char c : input.toCharArray()) {
-            if (isBase64Char(c)) {
-                result.append(c);
-            }
-        }
-
-        return encode(result.toString().getBytes());
-    }
-
-    private static boolean isBase64Char(char c) {
-        String base64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-        return base64Chars.indexOf(c) != -1;
-    }
 }
